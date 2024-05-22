a2f39e6cd616aa73d57b3aa52c9482dd3fca52fa
Erman Yalçın <ermanylcn1103@gmail.com>
2024-05-22 18:25:48 +0300

    Add project files.

---
+++ VisualProgrammingFinalProject/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace VisualProgrammingFinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbCities = new System.Windows.Forms.ComboBox();
+            this.cmbCounties = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbCities
+            // 
+            this.cmbCities.FormattingEnabled = true;
+            this.cmbCities.Location = new System.Drawing.Point(59, 268);
+            this.cmbCities.Name = "cmbCities";
+            this.cmbCities.Size = new System.Drawing.Size(121, 24);
+            this.cmbCities.TabIndex = 0;
+            this.cmbCities.SelectedIndexChanged += new System.EventHandler(this.cmbCities_SelectedIndexChanged);
+            // 
+            // cmbCounties
+            // 
+            this.cmbCounties.FormattingEnabled = true;
+            this.cmbCounties.Location = new System.Drawing.Point(245, 268);
+            this.cmbCounties.Name = "cmbCounties";
+            this.cmbCounties.Size = new System.Drawing.Size(121, 24);
+            this.cmbCounties.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbCounties);
+            this.Controls.Add(this.cmbCities);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCities;
+        private System.Windows.Forms.ComboBox cmbCounties;
+    }
+}
+
---
+++ VisualProgrammingFinalProject/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisualProgrammingFinalProject.Model;
+
+namespace VisualProgrammingFinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var cities = GetCities();
+            cmbCities.DataSource = cities;
+            cmbCities.DisplayMember = "title";
+            cmbCities.ValueMember = "id"; // Burada id doğru
+            cmbCities.SelectedIndex = -1; // İlk başta bir seçim olmasın
+        }
+
+        private void cmbCities_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbCities.SelectedValue != null && cmbCities.SelectedValue is int)
+            {
+                int cityId = (int)cmbCities.SelectedValue; // SelectedIndex yerine SelectedValue kullanarak id'yi alın
+                var counties = GetCountiesByCity(cityId);
+                cmbCounties.DataSource = counties;
+                cmbCounties.DisplayMember = "title";
+                cmbCounties.ValueMember = "id"; // Burada id doğru
+            }
+        }
+
+
+        #region Database Functions
+
+        #region Login Functions
+        public bool Login(string username, string password)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                // Assuming PasswordHash is already a hashed value
+                return context.Users.Any(user => user.Username == username && user.PasswordHash == password);
+            }
+        }
+
+        #endregion
+
+        #region Student Functions
+        public void AddStudent(Student student)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateStudent(Student student)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                context.Entry(student).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteStudent(int studentId)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                var student = context.Students.Find(studentId);
+                if (student != null)
+                {
+                    context.Students.Remove(student);
+                    context.SaveChanges();
+                }
+            }
+        }
+        public Student GetStudent(int studentId)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.Students.Find(studentId);
+            }
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.Students.ToList();
+            }
+        }
+        #endregion
+
+        #region Parent Functions
+        public void AddParent(Parent parent)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                context.Parents.Add(parent);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateParent(Parent parent)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                context.Entry(parent).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteParent(int parentId)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                var parent = context.Parents.Find(parentId);
+                if (parent != null)
+                {
+                    context.Parents.Remove(parent);
+                    context.SaveChanges();
+                }
+            }
+        }
+        public Parent GetParent(int parentId)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.Parents.Find(parentId);
+            }
+        }
+
+        public List<Parent> GetAllParents()
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.Parents.ToList();
+            }
+        }
+        #endregion
+
+        #region Cafeteria Functions
+        public List<CafeteriaPurchas> GetCafeteriaPurchases(int studentId)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.CafeteriaPurchases.Where(p => p.StudentID == studentId).ToList();
+            }
+        }
+        public bool MakePurchase(int studentId, int cafeteriaItemId, int quantity)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                var student = context.Students.Find(studentId);
+                var cafeteriaItem = context.CafeteriaItems.Find(cafeteriaItemId);
+
+                if (student != null && cafeteriaItem != null && cafeteriaItem.Stock >= quantity)
+                {
+                    decimal totalCost = cafeteriaItem.Price * quantity;
+                    if (student.Balance >= totalCost)
+                    {
+                        var purchase = new CafeteriaPurchas
+                        {
+                            StudentID = studentId,
+                            ItemID = cafeteriaItemId,
+                            Quantity = quantity,
+                            PurchaseDate = DateTime.Now
+                        };
+
+                        student.Balance -= totalCost;
+                        cafeteriaItem.Stock -= quantity;
+                        context.CafeteriaPurchases.Add(purchase);
+                        context.SaveChanges();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        public bool CanPurchase(int studentId, int cafeteriaItemId, int quantity)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                var student = context.Students.Find(studentId);
+                var cafeteriaItem = context.CafeteriaItems.Find(cafeteriaItemId);
+
+                if (student != null && cafeteriaItem != null)
+                {
+                    // Alışveriş kısıtlamalarını kontrol et
+                    var restrictions = context.PurchaseRestrictions
+                        .Where(r => r.StudentID == studentId && r.CafeteriaItemID == cafeteriaItemId)
+                        .FirstOrDefault();
+
+                    if (restrictions != null)
+                    {
+                        return false;
+                    }
+
+                    // Öğrencinin bakiyesinin yeterli olup olmadığını kontrol et
+                    decimal totalCost = cafeteriaItem.Price * quantity;
+                    if (student.Balance >= totalCost)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        #endregion
+
+        #region City and County Functions
+        public List<City> GetCities()
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.Cities.ToList();
+            }
+        }
+
+        public List<County> GetCountiesByCity(int cityId)
+        {
+            using (var context = new SchoolManagementSystemEntities())
+            {
+                return context.Counties.Where(c => c.city_id == cityId).ToList();
+            }
+        }
+
+        #endregion
+        #endregion
+    }
+}
